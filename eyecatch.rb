a43f43f7fd6c5002d01b89b31f99e042c3d414bf
y-kurami <yosuke.kurami@gmail.com>
2016-12-28 01:50:19 +0900

    modofy yarn install

--- eyecatch.rb
+++ eyecatch.rb
@@ -1,11 +1,10 @@
 serve 'ng build && http-server -p 8080'
 port 8080
 
-
 before_build {
   run 'ndenv install v6.9.1'
   run 'ndenv rehash'
   run 'ndenv global v6.9.1'
-  run 'npm install -g yarn'
+  run 'curl -o- -L https://yarnpkg.com/install.sh | bash'
   run 'yarn install'
 }
